e0ff3993d14be7b32f8e3dd5b5bccfc71b2b511f
TNK-git-hub <khanhpersonalwork@gmail.com>
2024-12-20 14:24:27 +0700

    learning java

---
+++ Chapter_1_quiz/src/quiz/ex1_1/ex1_1.java
@@ -0,0 +1,8 @@
+package quiz.ex1_1;
+
+public class ex1_1 {
+    public static void main (String[] args){
+        System.out.println("Welcom to Java");
+        System.out.println("Welcom to Computer Science");
+    }
+}
